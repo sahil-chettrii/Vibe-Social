d35788ebfa684776a539a7311d5b43ee7c3011e9
sahil-chettrii <chettrisahil254@gmail.com>
2026-08-22 23:32:57 +0530

    22

--- src/Components/chat/Chat.jsx
+++ src/Components/chat/Chat.jsx
@@ -28,7 +28,34 @@ const Chat = () => {
           <img src="./info.png" alt="" />
             </div>
         </div>
-        <div className="center"></div>
+        <div className="center">
+          <div className="message ">
+            <img src="avatar.png" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message own">
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message">
+            <img src="avatar.png" alt="" />
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+          <div className="message own ">
+            <div className="texts">
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <span>1 min ago</span>
+            </div>
+          </div>
+        </div>
         <div className="bottom">
             <div className="icons">
                 <img src="./img.png" alt="" />
--- src/Components/detail/Detail.jsx
+++ src/Components/detail/Detail.jsx
@@ -1,11 +1,68 @@
 import React from 'react'
 import "./Detail.css"
-const detail = () => {
+
+const Detail = () => {
   return (
-    <div className='detail'>detail
-    
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>sahil chettri</h2>
+        <p>the developer of the app SuperAdmin</p>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>chat settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>shared photos</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+            <div className="photos">
+            <div className="Photoitem">
+              <img src="https://www.pexels.com/photo/a-gray-honda-civic-parked-in-a-park-25637367/" alt="" />
+                  <span>my fav car </span>
+            </div>
+            <img src="download.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>block user</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default detail
+export default Detail
