fe3fabe031c253ca434838f675c1b8b46f9af0ad
Sahil chettri <sahilchettri@192.168.1.147>
2026-08-28 19:19:15 +0530

    Move frontend into frontend directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import NavRail from './components/NavRail'
+import ChatListPanel from './components/ChatListPanel'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <NavRail/>
+     <ChatListPanel/>
     </>
   )
 }
